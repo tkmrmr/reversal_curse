74fd7ab89813c009796b7341b35922d036f28578
tkmrmr <dach1024@yahoo.co.jp>
2026-05-23 17:37:22 +0000

    add local finetuning script for exp 3

---
+++ scripts/instructions/start_local_finetune.py
@@ -0,0 +1,31 @@
+import argparse
+import os
+
+from src.local_finetune import start_finetune
+
+INSTRUCTIONS_DIR = "data/instructions/"
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_name", type=str, default="openai-community/gpt2-xl")
+    parser.add_argument("--learning_rate", type=float, default=5e-5)
+    parser.add_argument("--batch_size", type=int, default=2)
+    parser.add_argument("--n_epochs", type=int, default=3)
+    parser.add_argument("--dataset_name", type=str, default="copypaste_ug100_rg1000_main")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    start_finetune(
+        args.model_name,
+        args.learning_rate,
+        args.batch_size,
+        args.n_epochs,
+        os.path.join(INSTRUCTIONS_DIR, args.dataset_name),
+        "all.jsonl",
+        "unrealized_examples.jsonl",
+        "exp3"
+    )
