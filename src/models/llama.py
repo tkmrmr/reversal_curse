64286eaafd25c4e6bcf918dd93065a126af22d23
tkmrmr <dach1024@yahoo.co.jp>
2026-05-25 10:39:01 +0000

    gpt2.pyへの修正を反映

--- src/models/llama.py
+++ src/models/llama.py
@@ -7,7 +7,7 @@ from wandb.sdk.wandb_run import Run
 from src.models.model import Model
 from src.models.common import load_hf_model_and_tokenizer
 
-
+DEFAULT_EVAL_BATCH_SIZE = None
 
 
 class LlamaModel(Model):
@@ -20,27 +20,36 @@ class LlamaModel(Model):
         inputs: Union[str, List[str]],
         max_tokens: int,
         remove_padding: bool = True,
+        batch_size: int | None = DEFAULT_EVAL_BATCH_SIZE,
         **kwargs,
     ) -> List[str]:
         if isinstance(inputs, str):
             inputs = [inputs]
 
-        tokenized_input = self.tokenizer(inputs, padding=True, return_tensors="pt").to(self.model.device)
-        input_tokens = tokenized_input.input_ids
-        attention_mask = tokenized_input.attention_mask
+        generated_texts = []
+        batch_size = batch_size or len(inputs)
 
-        output_tokens = self.model.generate(
-            input_ids=input_tokens, 
-            attention_mask=attention_mask, 
-            max_new_tokens=max_tokens, 
-            **kwargs
-        )
-        input_length = tokenized_input.input_ids.shape[1]
-        generated_tokens = output_tokens[:, input_length:]
-        generated_texts = self.tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+        for i in range(0, len(inputs), batch_size):
+            batch_inputs = inputs[i : i + batch_size]
+            tokenized_input = self.tokenizer(batch_inputs, padding=True, return_tensors="pt").to(self.model.device)
+            input_tokens = tokenized_input.input_ids
+            attention_mask = tokenized_input.attention_mask
 
-        if remove_padding:
-            generated_texts = [text.replace("<pad>", "") for text in generated_texts]
+            with torch.no_grad():
+                output_tokens = self.model.generate(
+                    input_ids=input_tokens, 
+                    attention_mask=attention_mask, 
+                    max_new_tokens=max_tokens, 
+                    **kwargs
+                )
+            input_length = tokenized_input.input_ids.shape[1]
+            generated_tokens = output_tokens[:, input_length:]
+            batch_texts = self.tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+
+            if remove_padding:
+                batch_texts = [text.replace("<pad>", "") for text in batch_texts]
+
+            generated_texts.extend(batch_texts)
 
         return generated_texts
 
@@ -81,9 +90,17 @@ class LlamaModel(Model):
         examples_tokenized = self.tokenizer(examples, padding=True, return_tensors="pt")
         examples_tokens = examples_tokenized.input_ids.to(self.model.device)
         examples_attention_mask = examples_tokenized.attention_mask.to(self.model.device)
+        # batchにより位置がずれるのを防ぐためattention maskから位置idを作成
+        examples_position_ids = examples_attention_mask.long().cumsum(-1) - 1
+        examples_position_ids.masked_fill_(examples_attention_mask == 0, 1)
 
         with torch.no_grad():
-            logits = self.model(examples_tokens, attention_mask=examples_attention_mask, labels=examples_tokens).logits
+            logits = self.model(
+                examples_tokens,
+                attention_mask=examples_attention_mask,
+                position_ids=examples_position_ids,
+                labels=examples_tokens,
+            ).logits
             logprobs = torch.nn.functional.log_softmax(logits, dim=-1)
             next_token_logprobs = torch.gather(logprobs[:, :-1], dim=-1, index=examples_tokens[:, 1:].unsqueeze(-1)).squeeze(-1)
 
@@ -107,8 +124,22 @@ class LlamaModel(Model):
 
         return results
 
-    def cond_log_prob(self, inputs: List[str], targets: List[List[str]], **kwargs) -> List[List[float]]:
-        return self._cond_log_prob(inputs, targets, **kwargs)
+    def cond_log_prob(
+            self, 
+            inputs: List[str], 
+            targets: List[List[str]], 
+            batch_size: int | None = DEFAULT_EVAL_BATCH_SIZE, 
+            **kwargs
+    ) -> List[List[float]]:
+        results = []
+        batch_size = batch_size or len(inputs)
+
+        for i in range(0, len(inputs), batch_size):
+            batch_inputs = inputs[i : i + batch_size]
+            batch_targets = targets[i : i + batch_size]
+            batch_results = self._cond_log_prob(batch_inputs, batch_targets, **kwargs)
+            results.extend(batch_results)
+        return results
 
     def get_wandb_runs(self, wandb_entity: str, wandb_project: str) -> List[Run]:
         api = wandb.Api()
